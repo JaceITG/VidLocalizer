1c2d35e70fcffad1189a9e630914bd765452f016
Scheisse <jjwazoo@gmail.com>
2022-10-02 18:41:59 -0400

    get channels to sweep

--- main.py
+++ main.py
@@ -5,19 +5,10 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 last_sweep = 0
+config = None
 
-def main(useConfig=None):
-    config = useConfig
 
-    if not config:
-        #Import config data from config.ini
-        from configparser import ConfigParser
-        config = ConfigParser()
-        config.read("config.ini")
-
-    client.run(config['SECRET']['Token'])
-
-#On bot startup/ready
+### On bot startup/ready ###
 @client.event
 async def on_ready():
     global last_sweep
@@ -27,5 +18,55 @@ async def on_ready():
     with open("log.txt", 'r') as f:
         cnt = f.read()
         last_sweep = time.gmtime(int(cnt.strip()))
-        print(last_sweep)
+        print(f'Last sweep for linked videos: {time.asctime(last_sweep)}')
 
+    await sweep()
+
+async def get_channels(server):
+    global config
+
+    try:
+        server_obj = await client.fetch_guild(server)
+    except discord.Forbidden:
+        print(f"Error: bot does not have access to server ID {server}")
+        return []
+    
+    channels = []
+    
+    #Generate list of channels to scan
+    if config['IDS']['Channel']:
+        #Use list of select channels
+        for channel in [int(c) for c in config['IDS']['Channel'].split(',')]:
+            if channel in [text_chan.id for text_chan in await server_obj.fetch_channels()]:
+                channels.append(channel)
+    else:
+        #Use all channels in guild
+        channels = [text_chan.id for text_chan in await server_obj.fetch_channels()]
+    
+    print(f"Server: {server_obj}\nChannels to scan:{channels}")
+    return channels
+
+
+async def sweep():
+    global config
+
+    #sweep in each server (converted to int ID)
+    for server in [int(s) for s in config['IDS']['Server'].split(',')]:
+        
+        channels = await get_channels(server)
+
+        #TODO: scan channel history up to last sweep point
+
+        
+
+def main(useConfig=None):
+    global config
+    config = useConfig
+
+    if not config:
+        #Import config data from config.ini
+        from configparser import ConfigParser
+        config = ConfigParser()
+        config.read("config.ini")
+
+    client.run(config['SECRET']['Token'])
